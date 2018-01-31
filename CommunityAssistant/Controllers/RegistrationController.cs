71d815e10efafb4bec40490bd93073b448ffb1dc
Ghada Messaoudi <ghada.messaoudi@seattlecentral.edu>
2018-01-31 12:15:37 -0700

    Assignment3: Registration

---
+++ CommunityAssistant/Controllers/RegistrationController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommunityAssistant.Models;
+
+namespace CommunityAssistant.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Result(Message m)
+        {
+            return View(m);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "LastName,FirstName,Email,Phone,PlainPassword,Apartment,Street,City,State,Zipcode")]NewPerson r)
+        {
+            int result = db.usp_Register(
+                r.LastName,
+                r.FirstName,
+                r.Email,
+                r.PlainPassword,
+                r.Apartment,
+                r.Street,
+                r.City,
+                r.State,
+                r.Zipcode,
+                r.Phone);
+            Message m = new Message();
+            if (result != -1)
+            {
+                m.MessageText = "Thank you for registring";
+                return RedirectToAction("Result",m);
+            }
+            
+            m.MessageText = "Error";
+            return RedirectToAction("Result",m);
+        }
+    }
+}
